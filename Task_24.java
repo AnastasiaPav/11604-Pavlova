66e7ac5a556c8c862cb919936a3f0c84fd3e02af
AnastasiaPav <pavlova_nastya1998@bk.ru>
2016-12-11 14:36:17 +0300

    Add files via upload

---
+++ Task_24.java
@@ -0,0 +1,14 @@
+public class Task_24 {
+	public static void main(String[] args) {
+		int n = Integer.parseInt(args[0]);	
+		for (int i = -n; i <= n; i++) {
+			for (int j = -n; j <= n; j++) {
+				if (i * i + j * j <= n * n + n / 3) 
+					System.out.print("* ");
+				else
+					System.out.print("  ");
+			}
+			System.out.println();
+		}
+	}
+}
