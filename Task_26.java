beb363dda4f885e2a92584289be00dfd14f57dfd
AnastasiaPav <pavlova_nastya1998@bk.ru>
2016-12-12 00:02:47 +0300

    29.09

--- Task_26.java
+++ Task_26.java
@@ -1,16 +1,53 @@
-public class Task_23 {
+public class Task_26 {
+
 	public static void main(String[] args) {
 		int n = Integer.parseInt(args[0]);
-		
-		for (int i = 0; i <= n/2; i++) {
-			for (int j = -n/2; j < n/2+1; j++) {
-				if (Math.abs(j) <= i) {
-					System.out.print("*");
-				} 	else 
-					System.out.print(" ");
+		int C;
+		int spaces = n;
+
+		for (int i = 0; i < n; i++) {
+			for (int j = spaces - 1; j >= 1; j--) {
+				System.out.print("   ");
 			}
-			System.out.println();
+
+			spaces--;
+			C = 1; 
+
+			if (C / 100 != 0) {
+				System.out.print(C + "   ");
+			}
+			else if (C / 10 != 0) {
+        			System.out.print("    ");
+        		}
+        		else if (C / 1000 != 0) {
+        			System.out.print("  ");
+        		}
+    			else {
+    				System.out.print(C + "     ");
+			     }
+    	
+    			for (int j = 1; j <= i; j++) {
+   	    	 		C *= (i - j + 1);
+   	    	 		C /= j;
+        			System.out.print(C);
+
+        			if (C / 100 != 0) {
+        				System.out.print("   ");
+        			}
+        			else if (C / 1000 != 0) {
+        				System.out.print("  ");
+        			}
+        			else if (C / 10 != 0) {
+        				System.out.print("    ");
+        			}
+        			else {
+        				System.out.print("     ");
+        			}
+        		
+    			}
+    
+    			System.out.println();
 		}
-		
 	}
+
 }
