7078f5ef812b42d1204ef4bcf14c4dbea7cb093e
AnastasiaPav <pavlova_nastya1998@bk.ru>
2016-12-29 12:44:55 +0300

    29.09

---
+++ Task_28.java
@@ -0,0 +1,34 @@
+public class Task_28 {
+	public static void main(String[] args) {
+		
+		String s1 = args[0];
+		String s2 = args[1];
+		boolean q = true;
+		
+		int minl = 0;
+		
+		if (s1.length() < s2.length()) {
+			minl = s1.length();
+		} 
+		else {
+			minl = s2.length();
+		}	
+		
+		for (int i = 0; i < minl; i++) {
+			if (s1.charAt(i) > s2.charAt(i)) {
+				System.out.println("s1 > s2");
+				q = false;
+				break;
+			}
+			else if (s1.charAt(i) < s2.charAt(i)){
+				System.out.println("s1 < s2");
+				q = false;
+				break;
+			}
+		}
+	
+		if (q) {
+			System.out.println("s1 = s2");
+		}
+	}
+}
